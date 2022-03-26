8a62f0899f0fd3fd55db8cea2894e54f7d97e464
Harrison <Harrison.Nicole@mayo.edu>
2022-03-26 11:42:29 -0500

    challenge

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // create a line from LAX to SFO by using coordinates for each point to be used in the line, then add more coordinates for additional stops
 let line = [
@@ -12,6 +12,35 @@ let line = [
   [47.4502, -122.3088]
 ];
 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng);
+    }
+
+  }).addTo(map);
+  
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
   color: "yellow"
